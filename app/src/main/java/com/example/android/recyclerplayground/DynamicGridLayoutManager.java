32b45df276936832efbadb13d4fc462f880928ac
Dave Smith <smith@wiresareobsolete.com>
2014-08-29 19:23:20 -0600

    Add experimental manager stubs.

---
+++ app/src/main/java/com/example/android/recyclerplayground/DynamicGridLayoutManager.java
@@ -0,0 +1,497 @@
+package com.example.android.recyclerplayground;
+
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.util.SparseArray;
+import android.view.View;
+
+public class DynamicGridLayoutManager extends RecyclerView.LayoutManager {
+
+    private static final String TAG = DynamicGridLayoutManager.class.getSimpleName();
+
+    /* Fill Direction Constants */
+    private static final int DIRECTION_NONE = -1;
+    private static final int DIRECTION_START = 0;
+    private static final int DIRECTION_END = 1;
+    private static final int DIRECTION_UP = 2;
+    private static final int DIRECTION_DOWN = 3;
+
+    /* First (top-left) position visible at any point */
+    private int mFirstVisiblePosition = 0;
+    /* Consistent size applied to all child views */
+    private int mDecoratedChildWidth;
+    private int mDecoratedChildHeight;
+    /* Number of columns that exist in the grid */
+    private int mTotalColumnCount;
+    /* Metrics for the visible window of our data */
+    private int mVisibleColumnCount;
+    private int mVisibleRowCount;
+
+    /*
+     * This method is your initial call from the framework. You will receive it when you
+     * need to start laying out the initial set of views. This method will not be called
+     * repeatedly, so don't rely on it to continually process changes during user
+     * interaction.
+     */
+    @Override
+    public void onLayoutChildren(RecyclerView.Recycler recycler, RecyclerView.State state) {
+        //We have nothing to show for an empty data set
+        if (getItemCount() == 0) return;
+        Log.d(TAG, "onLayoutChildren");
+        //Make the grid as square as possible, column count is root of the data set
+        mTotalColumnCount = (int) Math.round(Math.sqrt(getItemCount()));
+
+        //Scrap measure one child
+        View scrap = recycler.getViewForPosition(0);
+        addView(scrap);
+        measureChildWithMargins(scrap, 0, 0);
+
+        /*
+         * We make some assumptions in this code based on every child
+         * view being the same size (i.e. a uniform grid). This allows
+         * us to compute the following values up front because the
+         * won't change.
+         */
+        mDecoratedChildWidth = getDecoratedMeasuredWidth(scrap);
+        mDecoratedChildHeight = getDecoratedMeasuredHeight(scrap);
+
+        updateVisibleWindow(0, 0);
+
+        //Clear all attached views into the recycle bin
+        mFirstVisiblePosition = 0;
+        detachAndScrapAttachedViews(recycler);
+        //Fill the grid for the initial layout of views
+        fillGrid(DIRECTION_NONE, recycler, state);
+    }
+
+    @Override
+    public void onItemsAdded(RecyclerView recyclerView, int positionStart, int itemCount) {
+        //TODO: Monitor adapter changes
+        Log.i(TAG, "OnItemsAdded");
+        super.onItemsAdded(recyclerView, positionStart, itemCount);
+    }
+
+    @Override
+    public void onItemsRemoved(RecyclerView recyclerView, int positionStart, int itemCount) {
+        //TODO: Monitor adapter changes
+        Log.i(TAG, "OnItemsRemoved");
+        super.onItemsRemoved(recyclerView, positionStart, itemCount);
+    }
+
+    /*
+     * Based on the current scroll position offsets, compute how many
+     * rows/columns should be visible. This sliding window ensures that
+     * the bare minimum number of views necessary is attached at any point.
+     */
+    private void updateVisibleWindow(int startLeftOffset, int startTopOffset) {
+
+        mVisibleColumnCount = (getHorizontalSpace() - startLeftOffset) / mDecoratedChildWidth;
+        if (getHorizontalSpace() % mDecoratedChildWidth > 0) {
+            mVisibleColumnCount++;
+        }
+
+        //Allow minimum value for small data sets
+        if (mVisibleColumnCount > mTotalColumnCount) {
+            mVisibleColumnCount = mTotalColumnCount;
+        }
+        //Enforce maximum value
+        mVisibleColumnCount = Math.min(mTotalColumnCount - getFirstVisibleColumn(), mVisibleColumnCount);
+
+        mVisibleRowCount = (getVerticalSpace() - startTopOffset) / mDecoratedChildHeight;
+        if (getVerticalSpace() % mDecoratedChildHeight > 0) {
+            mVisibleRowCount++;
+        }
+    }
+
+    private void fillGrid(int direction, RecyclerView.Recycler recycler, RecyclerView.State state) {
+        //Always range check visible position
+        if (mFirstVisiblePosition < 0) mFirstVisiblePosition = 0;
+        if (mFirstVisiblePosition > getItemCount()) mFirstVisiblePosition = getItemCount();
+
+        /*
+         * First, we will detach all existing views from the layout.
+         * detachView() is a lightweight operation that we can use to
+         * quickly reorder views without a full add/remove.
+         */
+        SparseArray<View> viewCache = new SparseArray<View>(getChildCount());
+        int startLeftOffset = getPaddingLeft();
+        int startTopOffset = getPaddingTop();
+        if (getChildCount() != 0) {
+            final View topView = getChildAt(0);
+            startLeftOffset = getDecoratedLeft(topView);
+            startTopOffset = getDecoratedTop(topView);
+            switch (direction) {
+                case DIRECTION_START:
+                    startLeftOffset -= mDecoratedChildWidth;
+                    break;
+                case DIRECTION_END:
+                    startLeftOffset += mDecoratedChildWidth;
+                    break;
+                case DIRECTION_UP:
+                    startTopOffset -= mDecoratedChildHeight;
+                    break;
+                case DIRECTION_DOWN:
+                    startTopOffset += mDecoratedChildHeight;
+                    break;
+            }
+
+            //Cache all views by their existing position, before updating counts
+            for (int i=0; i < getChildCount(); i++) {
+                int position = positionOfIndex(i);
+                final View child = getChildAt(i);
+                viewCache.put(position, child);
+            }
+
+            //Update currently visible counts before detaching views
+            updateVisibleWindow(startLeftOffset, startTopOffset);
+
+            //Temporarily detach all views.
+            // Views we still need will be added back at the proper index.
+            for (int i=0; i < viewCache.size(); i++) {
+                detachView(viewCache.valueAt(i));
+            }
+        }
+
+        /*
+         * Next, we advance the visible position based on the fill direction.
+         * DIRECTION_NONE doesn't advance the position in any direction.
+         */
+        switch (direction) {
+            case DIRECTION_START:
+                mFirstVisiblePosition--;
+                break;
+            case DIRECTION_END:
+                mFirstVisiblePosition++;
+                break;
+            case DIRECTION_UP:
+                mFirstVisiblePosition -= getTotalColumnCount();
+                break;
+            case DIRECTION_DOWN:
+                mFirstVisiblePosition += getTotalColumnCount();
+                break;
+        }
+
+        /*
+         * Next, we supply the grid of items that are deemed visible.
+         * If these items were previously there, they will simple be
+         * re-attached. New views that must be created are obtained
+         * from the Recycler and added.
+         */
+        int leftOffset = startLeftOffset;
+        int topOffset = startTopOffset;
+
+        for (int i = 0; i < getVisibleChildCount(); i++) {
+            int nextPosition = positionOfIndex(i);
+
+            if (nextPosition >= getItemCount()) {
+                //Item space beyond the data set, don't attempt to add a view
+                continue;
+            }
+
+            //Layout this position
+            View view = viewCache.get(nextPosition);
+            if (view == null) {
+                /*
+                 * The Recycler will give us either a newly constructed view,
+                 * or a recycled view it has on-hand. In either case, the
+                 * view will already be fully bound to the data by the
+                 * adapter for us.
+                 */
+                view = recycler.getViewForPosition(nextPosition);
+                addView(view);
+
+                /*
+                 * It is prudent to measure/layout each new view we
+                 * receive from the Recycler. We don't have to do
+                 * this for views we are just re-arranging.
+                 */
+                measureChildWithMargins(view, 0, 0);
+                layoutDecorated(view, leftOffset, topOffset,
+                        leftOffset + mDecoratedChildWidth,
+                        topOffset + mDecoratedChildHeight);
+            } else {
+                //Re-attach the cached view at its new index
+                attachView(view);
+                viewCache.remove(nextPosition);
+            }
+
+            if (i % mVisibleColumnCount == (mVisibleColumnCount - 1)) {
+                leftOffset = startLeftOffset;
+                topOffset += mDecoratedChildHeight;
+                //If we wrapped without setting the column count, we've reached it
+            } else {
+                leftOffset += mDecoratedChildWidth;
+            }
+        }
+
+        /*
+         * Finally, we ask the Recycler to scrap and store any views
+         * that we did not re-attach. These are views that are not currently
+         * necessary because they are no longer visible.
+         */
+        for (int i=0; i < viewCache.size(); i++) {
+            recycler.recycleView(viewCache.valueAt(i));
+        }
+    }
+
+    @Override
+    public void scrollToPosition(int position) {
+        //TODO: Handle programmatic scrolling
+        super.scrollToPosition(position);
+    }
+
+    @Override
+    public void smoothScrollToPosition(RecyclerView recyclerView, RecyclerView.State state, int position) {
+        //TODO: Handle programmatic scrolling
+        super.smoothScrollToPosition(recyclerView, state, position);
+    }
+
+    /*
+     * Use this method to tell the RecyclerView if scrolling is even possible
+     * in the horizontal direction.
+     */
+    @Override
+    public boolean canScrollHorizontally() {
+        //We do allow scrolling
+        return true;
+    }
+
+    /*
+     * This method describes how far RecyclerView thinks the contents should scroll horizontally.
+     * You are responsible for verifying edge boundaries, and determining if this scroll
+     * event somehow requires that new views be added or old views get recycled.
+     */
+    @Override
+    public int scrollHorizontallyBy(int dx, RecyclerView.Recycler recycler, RecyclerView.State state) {
+        if (getChildCount() == 0) {
+            return 0;
+        }
+
+        //Take leftmost measurements from the top-left child
+        final View topView = getChildAt(0);
+        //Take rightmost measurements from the top-right child
+        final View bottomView = getChildAt(mVisibleColumnCount-1);
+        int delta;
+        boolean leftBoundReached = getFirstVisibleColumn() == 0;
+        boolean rightBoundReached = getLastVisibleColumn() >= getTotalColumnCount();
+        if (dx > 0) { // Contents are scrolling left
+            //Check right bound
+            if (rightBoundReached) {
+                //If we've reached the last column, enforce limits
+                int rightOffset = getHorizontalSpace() - getDecoratedRight(bottomView) + getPaddingRight();
+                delta = Math.max(-dx, rightOffset);
+            } else {
+                //No limits while the last column isn't visible
+                delta = -dx;
+            }
+        } else { // Contents are scrolling right
+            //Check left bound
+            if (leftBoundReached) {
+                int leftOffset = -getDecoratedLeft(topView) + getPaddingLeft();
+                delta = Math.min(-dx, leftOffset);
+            } else {
+                delta = -dx;
+            }
+        }
+
+        offsetChildrenHorizontal(delta);
+
+        if (dx > 0) {
+            if (getDecoratedRight(topView) < 0 && !rightBoundReached) {
+                fillGrid(DIRECTION_END, recycler, state);
+            } else if (!rightBoundReached) {
+                fillGrid(DIRECTION_NONE, recycler, state);
+            }
+        } else {
+            if (getDecoratedLeft(topView) > 0 && !leftBoundReached) {
+                fillGrid(DIRECTION_START, recycler, state);
+            } else if (!leftBoundReached) {
+                fillGrid(DIRECTION_NONE, recycler, state);
+            }
+        }
+
+        /*
+         * Return value determines if a boundary has been reached
+         * (for edge effects and flings). If returned value does not
+         * match original delta (passed in), RecyclerView will draw
+         * an edge effect.
+         */
+        return (Math.abs(delta) != Math.abs(dx)) ? Math.abs(delta) : dx;
+    }
+
+    /*
+     * Use this method to tell the RecyclerView if scrolling is even possible
+     * in the vertical direction.
+     */
+    @Override
+    public boolean canScrollVertically() {
+        //We do allow scrolling
+        return true;
+    }
+
+    /*
+     * This method describes how far RecyclerView thinks the contents should scroll vertically.
+     * You are responsible for verifying edge boundaries, and determining if this scroll
+     * event somehow requires that new views be added or old views get recycled.
+     */
+    @Override
+    public int scrollVerticallyBy(int dy, RecyclerView.Recycler recycler, RecyclerView.State state) {
+        if (getChildCount() == 0) {
+            return 0;
+        }
+
+        //Take top measurements from the top-left child
+        final View topView = getChildAt(0);
+        //Take bottom measurements from the bottom-right child.
+        final View bottomView = getChildAt(getChildCount()-1);
+        int delta;
+        int maxRowCount = getTotalRowCount();
+        boolean topBoundReached = getFirstVisibleRow() == 0;
+        boolean bottomBoundReached = getLastVisibleRow() >= maxRowCount;
+        if (dy > 0) { // Contents are scrolling up
+            //Check against bottom bound
+            if (bottomBoundReached) {
+                //If we've reached the last row, enforce limits
+                int bottomOffset;
+                if (rowOfIndex(getChildCount() - 1) >= (maxRowCount - 1)) {
+                    //We are truly at the bottom, determine how far
+                    bottomOffset = getVerticalSpace() - getDecoratedBottom(bottomView)
+                            + getPaddingBottom();
+                } else {
+                    /*
+                     * Extra space added to account for allowing bottom space in the grid.
+                     * This occurs when the overlap in the last row is not large enough to
+                     * ensure that at least one element in that row isn't fully recycled.
+                     */
+                    bottomOffset = getVerticalSpace() - (getDecoratedBottom(bottomView)
+                            + mDecoratedChildHeight) + getPaddingBottom();
+                }
+
+                delta = Math.max(-dy, bottomOffset);
+            } else {
+                //No limits while the last row isn't visible
+                delta = -dy;
+            }
+        } else { // Contents are scrolling down
+            //Check against top bound
+            if (topBoundReached) {
+                int topOffset = -getDecoratedTop(topView) + getPaddingTop();
+
+                delta = Math.min(-dy, topOffset);
+            } else {
+                delta = -dy;
+            }
+        }
+
+        offsetChildrenVertical(delta);
+
+        if (dy > 0) {
+            if (getDecoratedBottom(topView) < 0 && !bottomBoundReached) {
+                fillGrid(DIRECTION_DOWN, recycler, state);
+            } else if (!bottomBoundReached) {
+                fillGrid(DIRECTION_NONE, recycler, state);
+            }
+        } else {
+            if (getDecoratedTop(topView) > 0 && !topBoundReached) {
+                fillGrid(DIRECTION_UP, recycler, state);
+            } else if (!topBoundReached) {
+                fillGrid(DIRECTION_NONE, recycler, state);
+            }
+        }
+
+        /*
+         * Return value determines if a boundary has been reached
+         * (for edge effects and flings). If returned value does not
+         * match original delta (passed in), RecyclerView will draw
+         * an edge effect.
+         */
+        return (Math.abs(delta) != Math.abs(dy)) ? Math.abs(delta) : dy;
+    }
+
+    /*
+     * We must override this method to provide the default layout
+     * parameters that each child view will receive when added.
+     */
+    @Override
+    public RecyclerView.LayoutParams generateDefaultLayoutParams() {
+        return new RecyclerView.LayoutParams(
+                RecyclerView.LayoutParams.WRAP_CONTENT,
+                RecyclerView.LayoutParams.WRAP_CONTENT);
+    }
+
+    /*
+     * This is a helper method used by RecyclerView to determine
+     * if a specific child view can be returned.
+     */
+    @Override
+    public View findViewByPosition(int position) {
+        for (int i=0; i < getChildCount(); i++) {
+            if (positionOfIndex(i) == position) {
+                return getChildAt(i);
+            }
+        }
+
+        return null;
+    }
+
+    /** Private Helpers and Metrics Accessors */
+
+    /*
+     * Mapping between child view indices and adapter data
+     * positions helps fill the proper views during scrolling.
+     */
+    private int positionOfIndex(int childIndex) {
+        int row = childIndex / mVisibleColumnCount;
+        int column = childIndex % mVisibleColumnCount;
+
+        return mFirstVisiblePosition + (row * getTotalColumnCount()) + column;
+    }
+
+    private int rowOfIndex(int childIndex) {
+        int position = positionOfIndex(childIndex);
+
+        return position / getTotalColumnCount();
+    }
+
+    private int getFirstVisibleColumn() {
+        return (mFirstVisiblePosition % getTotalColumnCount());
+    }
+
+    private int getLastVisibleColumn() {
+        return getFirstVisibleColumn() + mVisibleColumnCount;
+    }
+
+    private int getFirstVisibleRow() {
+        return (mFirstVisiblePosition / getTotalColumnCount());
+    }
+
+    private int getLastVisibleRow() {
+        return getFirstVisibleRow() + mVisibleRowCount;
+    }
+
+    private int getVisibleChildCount() {
+        return mVisibleColumnCount * mVisibleRowCount;
+    }
+
+    private int getTotalColumnCount() {
+        return mTotalColumnCount;
+    }
+
+    private int getTotalRowCount() {
+        int maxRow = getItemCount() / mTotalColumnCount;
+        //Bump the row count if it's not exactly even
+        if (getItemCount() % mTotalColumnCount != 0) {
+            maxRow++;
+        }
+
+        return maxRow;
+    }
+
+    private int getHorizontalSpace() {
+        return getWidth() - getPaddingRight() - getPaddingLeft();
+    }
+
+    private int getVerticalSpace() {
+        return getHeight() - getPaddingBottom() - getPaddingTop();
+    }
+}
